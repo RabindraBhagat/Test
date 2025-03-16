f9591a6c4fc9ec8f3f077cea90029ad02a190f9a
Rabindra Bhagat <rbnbhagat01@gmail.com>
2025-03-16 08:07:55 +0530

    Nothing Now

---
+++ JS.js
@@ -0,0 +1,22 @@
+// Switch-Case
+
+const option = 1;
+switch (option) {
+  case 1:
+    console.log("Namaste!");
+    break;
+  case 2:
+    1;
+    console.log("Hello");
+    break;
+  case 3:
+    console.log("Hola");
+    break;
+  default:
+    console.log("Invalid Option.");
+}
+
+// For Loop
+for (let i = 1; i <= 10; i = i + 1) {
+  console.log("Rabindra" + i);
+}
